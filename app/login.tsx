a2b6f6f200880c13ea1dec93026e8ede683cd8f4
LuigiRechine <lbarrosrechinelli@gmail.com>
2026-03-26 16:45:42 -0300

    Login

---
+++ app/login.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
+import styles from './css/style';
+import { Link } from 'expo-router';
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+        <View>
+            <Image style={styles.img} source={require("../assets/sesisenai.png")}></Image>
+        </View>
+        <View style={styles.form}>
+            <Text style={styles.esquerda}>Login</Text>
+            <TextInput placeholder='Insira seu CPF' style={styles.input}/>
+            <Text style={styles.esquerda}>Senha</Text>
+            <TextInput placeholder='Insira seu CPF' style={styles.input}/>
+            <Link href={''} style={styles.direita}>Cadastrar</Link>
+            <Link href={''} style={styles.direita}>Esqueci a senha</Link>
+        </View>
+        <View>
+            <Link href={''} style={styles.btn}>ENTRAR</Link>
+        </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
