a099ef7cb576e1a4d63c3a5585a88d3df5483063
LuigiRechine <lbarrosrechinelli@gmail.com>
2026-04-23 11:30:10 -0300

    create cadastro e esqueciSenha

--- app/css/style.tsx
+++ app/css/style.tsx
@@ -3,52 +3,63 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    flexDirection:'column',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  input:{
+  input: {
     padding: 10,
-    margin:10,
-    borderColor:"#ccc",
+    margin: 10,
+    borderColor: "#ccc",
     borderWidth: 1,
-    borderRadius:10,
-    width:350,
+    borderRadius: 10,
+    width: 350,
   },
-  direita:{
-    width:350,
-    display:'flex',
-    justifyContent:'flex-start',
+  esquerda: {
+    width: 350,
+    textAlign: "left",
   },
-  esquerda:{
-    width:350,
-    display:'flex',
-    justifyContent:'flex-end',
+  direita: {
+    width: 350,
+    textAlign: "right",
+    fontWeight:'bold'
   },
-  form:{
-    display: 'flex',
-    justifyContent:'center',
-    width:350,
+  form: {
+    justifyContent: "center",
+    width: 350,
+    marginTop: 30,
   },
-  btn:{
-    width:150,
-    height:50,
-    backgroundColor:'blue',
-    borderRadius:10,
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
-    color:'white'
+  btn: {
+    width: 200,
+    height: 50,
+    backgroundColor: "#dd0000",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
   },
-  img:{
-    width:300,
-    height:45,
+  img: {
+    width: 300,
+    height: 45,
+    marginTop: 40,
   },
-  baixo:{
-    display:'flex',
-    
-  }
+  baixo: {
+    flexDirection: "column",
+    alignItems: "flex-end",
+  },
+  entrar: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  contBaixo: {
+    width: 350,
+    height: 40,
+    justifyContent: "flex-end",
+    marginTop: 10,
+  },
+  botao: {
+    marginTop: 20,
+  },
 });
 
 export default styles;
